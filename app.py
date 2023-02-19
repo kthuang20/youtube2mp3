16116524e75cd4976a4486d7ec86514c148412de
Katie Huang <125783075+kthuang20@users.noreply.github.com>
2023-02-19 15:58:03 +0000

    added requirements

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+YouTubeURLS = [
+    {
+        'id': 1,
+        'title': 'YouTube URL 1'
+    },
+    {
+        'id': 2,
+        'title': 'YouTube URL 2'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', urls=YouTubeURLS)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
